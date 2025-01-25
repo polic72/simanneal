e5307b513a59df420fb53e866e14590458d1db5f
Garrett Stonis <stonisg@outlook.com>
2025-01-25 00:54:16 -0500

    Project setup and starting because I forgor.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        double x = 
+
+        Console.WriteLine("Hello, World!");
+    }
+
+
+    public static double f(double x)
+    {
+        return x*x*x*x - 2*x*x + x;
+    }
+
+
+    public static double deriv(Func<double, double> f, double x, double h)
+    {
+        return (f(x + h) - f(x - h)) / (2 * h);
+    }
+}
